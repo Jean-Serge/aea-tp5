f2fba5433f834c8d4169534051497d6a5c53ff9f
Verbaere Thibaud <verbaere@a12p11.fil.univ-lille1.fr>
2015-03-27 10:23:57 +0100

    ajout d'une classe Welsh-Powel

---
+++ src/algo/WelshPowel.java
@@ -0,0 +1,30 @@
+package algo;
+
+import entite.Graphe;
+
+/**
+ * Classe modélisant la coloration selon l'algorithme de Welsh-Powel.
+ * @author verbaere
+ *
+ */
+public class WelshPowel {
+	
+	private Graphe graphe;
+
+	/**
+	 * Créé une nouvelle instance pour une colorisation selon Welsh-Powel d'un graphe.
+	 * @param graphe le graphe à colorer.
+	 */
+	public WelshPowel(Graphe graphe) {
+		this.graphe = graphe;
+	}
+	
+	/**
+	 * Execute l'algorithme de Welsh-Powel sur le graphe.
+	 * @return le nombre de couleur utilisé durant l'algorithme
+	 */
+	public int execute() {
+		return 0;
+	}
+	
+}
