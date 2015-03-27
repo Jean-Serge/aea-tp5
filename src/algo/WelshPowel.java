23ec09531b9a34a2234441cb22530e08c7e37782
Verbaere Thibaud <verbaere@a12p11.fil.univ-lille1.fr>
2015-03-27 11:21:55 +0100

    v1 de WelshPowel

--- src/algo/WelshPowel.java
+++ src/algo/WelshPowel.java
@@ -1,6 +1,12 @@
 package algo;
 
+import java.util.Collections;
+import java.util.List;
+
+import utils.DegreSommetComparator;
+
 import entite.Graphe;
+import entite.Sommet;
 
 /**
  * Classe modélisant la coloration selon l'algorithme de Welsh-Powel.
@@ -10,6 +16,8 @@ import entite.Graphe;
 public class WelshPowel {
 	
 	private Graphe graphe;
+	
+	private int nb_couleur;
 
 	/**
 	 * Créé une nouvelle instance pour une colorisation selon Welsh-Powel d'un graphe.
@@ -17,6 +25,7 @@ public class WelshPowel {
 	 */
 	public WelshPowel(Graphe graphe) {
 		this.graphe = graphe;
+		this.nb_couleur = 0;
 	}
 	
 	/**
@@ -24,7 +33,23 @@ public class WelshPowel {
 	 * @return le nombre de couleur utilisé durant l'algorithme
 	 */
 	public int execute() {
-		return 0;
+		List<Sommet> sommets = graphe.getSommets();
+		// on tri la liste des sommets
+		Collections.sort(sommets,new DegreSommetComparator());
+
+		for (Sommet s: sommets) {
+			
+			if (s.getCouleur() == 0) {
+				
+				for (Sommet s1 : sommets) {
+					if (!s1.getVoisins().contains(s))
+						s1.colorerSommet(this.nb_couleur+1);
+				}
+				
+			}
+		}
+			
+		return this.nb_couleur;
 	}
 	
 }
